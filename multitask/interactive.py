cd5b3c52117d09f7766b5f83a946bdd7bcab863f
Yu Fang <squarefk@gmail.com>
2021-05-15 19:53:37 +0800

    fix interactive

--- multitask/interactive.py
+++ multitask/interactive.py
@@ -2,6 +2,7 @@ import config
 config.batch_size = 1
 import multitask
 import taichi as ti
+import os
 
 offset = 0
 def set_target():
@@ -20,19 +21,19 @@ def set_target():
             set_target.target_h = 0.1
     print("Status: ", set_target.target_v, set_target.target_h)
     multitask.initialize_interactive(1, set_target.target_v, set_target.target_h)
-set_target.target_v = -0.06
+set_target.target_v = 0
 set_target.target_h = 0.1
 
 def make_decision():
     multitask.nn.clear_single(0)
-    multitask.compute_center(0)
+    multitask.solver.compute_center(0)
     multitask.nn_input(0, offset, 0.08, 0.1)
     multitask.nn.forward(0)
 
 def forward_mass_spring():
-    multitask.apply_spring_force(0)
-    multitask.advance_toi(1)
-    multitask.clear_states(1)
+    multitask.solver.apply_spring_force(0)
+    multitask.solver.advance_toi(1)
+    multitask.solver.clear_states(1)
 
 @ti.kernel
 def refresh_xv():
@@ -47,32 +48,13 @@ def visualizer():
     gui.line((0, multitask.ground_height), (1, multitask.ground_height),
              color=0x000022,
              radius=3)
-    def circle(x, y, color):
-        gui.circle((x, y), ti.rgb_to_hex(color), 7)
-    for i in range(multitask.n_springs):
-        def get_pt(x):
-            return (x[0], x[1])
-        a = multitask.actuation[0, 0, i] * 0.5
-        r = 2
-        if multitask.spring_actuation[i] == 0:
-            a = 0
-            c = 0x222222
-        else:
-            r = 4
-            c = ti.rgb_to_hex((0.5 + a, 0.5 - abs(a), 0.5 - a))
-        gui.line(get_pt(multitask.x[0, 0, multitask.spring_anchor_a[i]]),
-                 get_pt(multitask.x[0, 0, multitask.spring_anchor_b[i]]),
-                 color=c,
-                 radius=r)
-    for i in range(multitask.n_objects):
-        color = (0.06640625, 0.06640625, 0.06640625)
-        circle(multitask.x[0, 0, i][0], multitask.x[0, 0, i][1], color)
-    gui.show('mass_spring/{:04d}.png'.format(visualizer.frame))
+    multitask.solver.draw_robot(gui, 0, multitask.target_v)
+    gui.show('video/interactive/{:04d}.png'.format(visualizer.frame))
     visualizer.frame += 1
 visualizer.frame = 0
-import os
+
 if __name__ == "__main__":
-    os.makedirs("mass_spring", exist_ok = True)
+    os.makedirs("video/interactive", exist_ok = True)
     multitask.setup_robot()
     multitask.nn.load_weights("robot_{}/weight.pkl".format(config.robot_id))
     print(multitask.x.to_numpy()[0, :, :])
