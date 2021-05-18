38a2f407f37ac4b125119ddefc0a24f9076943f7
leon <leonz5288@hotmail.com>
2021-05-18 13:04:33 +0800

    speed as state

--- multitask/multitask_rl.py
+++ multitask/multitask_rl.py
@@ -1,5 +1,6 @@
 import gym
 from gym import spaces
+from numpy.random import get_state
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3 import PPO
 import torch
@@ -13,7 +14,7 @@ import matplotlib.pyplot as plt
 import taichi as ti
 import numpy as np
 
-from taichi.lang.ops import mul
+from taichi.lang.ops import mul, sin
 
 class MassSpringEnv(gym.Env):
     metadata = {'render.modes': ['human']}
@@ -22,7 +23,7 @@ class MassSpringEnv(gym.Env):
         super(MassSpringEnv, self).__init__()
         self.act_spring = [0, 5, 6, 10, 15, 20, 21, 26, 30]
         self.action_space = spaces.Box(low=-1, high=1, shape=(9, ), dtype=np.float64)
-        self.observation_space = spaces.Box(low=-5, high=5, shape=(n_input_states, ), dtype=np.float64)
+        self.observation_space = spaces.Box(low=-1, high=1, shape=(1, ), dtype=np.float64)
         self.rollout_lenth = 1000
         multitask.setup_robot()
         self.t = 0
@@ -35,10 +36,10 @@ class MassSpringEnv(gym.Env):
         multitask.solver.advance_toi(self.t+1)
         multitask.solver.compute_center(self.t+1)
         multitask.solver.compute_height(self.t+1)
-        multitask.nn_input(self.t+1, 0, 0.8, 0.2)
-        # multitask.nn_input(self.t+1, 0, 0.08, 0.1)
-        observation = multitask.input_state.to_numpy()[self.t+1, 0]
-        reward = self.get_reward()
+        # multitask.nn_input(self.t+1, 0, 0.8, 0.2)
+        # observation = multitask.input_state.to_numpy()[self.t+1, 0]
+        reward = self.get_state()
+        observation = np.array([reward])
 
         self.t += 1
 
@@ -51,28 +52,24 @@ class MassSpringEnv(gym.Env):
         info = {}
         return observation, reward, done, info
 
-    def get_reward(self):
-        reward = 0.
-        if self.t > 100:
-            pos = multitask.solver.center.to_numpy()
-            pos1 = pos[self.t-100, 0][0]
-            pos2 = pos[self.t, 0][0]
-            height = multitask.solver.height.to_numpy()[self.t, 0]
-            # reward -= (0.2-height) ** 2
-            # reward -= (0.1-pos) ** 2
-            # reward = height/500
-            reward = (pos2-pos1)
+    def get_state(self):
+        pos = multitask.solver.center.to_numpy()
+        pos1 = pos[max(self.t-100, 0), 0][0]
+        pos2 = pos[self.t, 0][0]
+        # height = multitask.solver.height.to_numpy()[self.t, 0]
+        # reward -= (0.2-height) ** 2
+        # reward -= (0.1-pos) ** 2
+        # reward = height/500
+        reward = (pos2-pos1)
         return reward
 
     def reset(self):
         self.t = 0
         multitask.solver.clear_states(self.rollout_lenth)
-        multitask.nn_input(self.t, 0, 0.8, 0.2)
+        # multitask.nn_input(self.t, 0, 0.8, 0.2)
         multitask.solver.compute_center(self.t)
         multitask.solver.compute_height(self.t)
-        pos = multitask.solver.center.to_numpy()[self.t, 0][0]
-        height = multitask.solver.height.to_numpy()[self.t, 0]
-        observation = multitask.input_state.to_numpy()[self.t, 0]
+        observation = np.array([self.get_state()])
         return observation
     
     def render(self, mode):
@@ -90,10 +87,10 @@ def visualizer(t):
 visualizer.frame = 0
 
 env = MassSpringEnv()
-sine_fun = lambda : torch.sin
-policy_kwargs = dict(activation_fn=sine_fun, net_arch=[dict(vf=[64, 64])])
-model = PPO('MlpPolicy', env, gamma=0.99, learning_rate=1e-3, verbose=1)
-model.learn(total_timesteps=200000)
+check_env(env)
+policy_kwargs = dict(activation_fn=torch.nn.Tanh, net_arch=[dict(vf=[64, 64])])
+model = PPO('MlpPolicy', env, gamma=1, learning_rate=1e-3, verbose=1, policy_kwargs=policy_kwargs)
+model.learn(total_timesteps=2000000)
 
 # multitask.setup_robot()
 obs = env.reset()
