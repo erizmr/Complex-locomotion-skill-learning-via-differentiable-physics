8cd2244581161feb03f7d8229350685429bc612b
Mingrui <mingrui.zhang@taichi.graphics>
2021-08-06 17:05:36 +0800

    update cfg

--- interactive.py
+++ interactive.py
@@ -7,6 +7,9 @@ from multitask.config_sim import ConfigSim
 from multitask.diffphy_trainer import DiffPhyTrainer
 # from solver_mass_spring import SolverMassSpring
 # from solver_mpm import SolverMPM
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 offset = 0
 def set_target():
@@ -43,6 +46,11 @@ set_target.target_c = 0.0
 
 
 all_holder = []
+lower_height_holder = []
+velocity_holder = []
+upper_height_holder = []
+target_v_holder = []
+center_holder = []
 def make_decision():
     trainer.nn.clear_single(0)
     trainer.taichi_env.solver.compute_center(0)
@@ -51,11 +59,24 @@ def make_decision():
         trainer.nn.forward(0)
 
     holder = []
+    v_sub_holder = []
     for i in range(trainer.taichi_env.solver.n_objects):
         holder.append(round(trainer.taichi_env.solver.actuation[0, 0, i], 3))
+        v_sub_holder.append(trainer.taichi_env.v[0, 0, i][0])
     if offset % int(control_length) == 0:
         print(f"Frame: {offset}, control signal: {holder}")
     all_holder.append(holder)
+
+    if set_target.target_c == 1.0:
+        target_v_holder.append(set_target.target_v / 2)
+    else:
+        target_v_holder.append(set_target.target_v)
+    # velocity_holder.append(np.mean(v_sub_holder))
+
+    center_holder.append(trainer.taichi_env.center[0, 0][0])
+    velocity_holder.append(trainer.taichi_env.center[0, 0][0] - center_holder[max(offset, 100) - 100])
+    lower_height_holder.append(trainer.taichi_env.height[0, 0])
+    upper_height_holder.append(trainer.taichi_env.upper_height[0, 0])
 
 def forward_mass_spring():
     trainer.taichi_env.solver.apply_spring_force(0)
@@ -75,9 +96,12 @@ def visualizer():
     gui.line((0, trainer.taichi_env.ground_height), (1, trainer.taichi_env.ground_height),
              color=0x000022,
              radius=3)
+    gui.text(f"Robot ID: {robot_id}", (0.05, 0.9), color=0x000022, font_size=20)
+    gui.text(f"Control length: {control_length}", (0.05, 0.85), color=0x000022, font_size=20)
+    gui.text(f"Targets v: {set_target.target_v:.2f}, h: {set_target.target_h:.2f}, c: {set_target.target_c:.2f}", (0.05, 0.80), color=0x000022, font_size=20)
     trainer.taichi_env.solver.draw_robot(gui=gui, batch_rank=1, t=1, target_v=trainer.taichi_env.target_v)
-    # gui.show('video/interactive/{:04d}.png'.format(visualizer.frame))
-    gui.show()
+    gui.show('video/interactive/robot_{}/{}/{:04d}.png'.format(robot_id, config_name, visualizer.frame))
+    # gui.show()
     visualizer.frame += 1
 visualizer.frame = 0
 
@@ -87,8 +111,11 @@ if __name__ == "__main__":
     print('args', args)
     config_file = args.config_file
     config = ConfigSim.from_file(config_file, if_mkdir=False)
-
+    config_name = config_file.split('/')[-1].split('.')[0]
+    robot_id = config.get_config()["robot"]["robot_id"]
+    os.makedirs(f'./video/interactive/robot_{robot_id}/{config_name}', exist_ok=True)
     control_length = config.get_config()["robot"]["control_length"]
+
 
     # Enforce the batch size to 1
     config._config["nn"]["batch_size"] = 1
@@ -112,11 +139,63 @@ if __name__ == "__main__":
             refresh_xv()
             offset += 1
         visualizer()
-    import matplotlib.pyplot as plt
-    import numpy as np
+        # if offset < 100:
+        #     set_target.target_v = 0.0
+        #     set_target.target_h = 0.1
+        #     set_target.target_c = 0.0
+        # if offset > 100 and offset <= 500:
+        #     set_target.target_v = 0.01
+        #     set_target.target_h = 0.1
+        #     set_target.target_c = 0.0
+        # if offset > 500 and offset < 2000:
+        #     set_target.target_v = 0.015 * (offset // 500)
+        #     set_target.target_h = 0.1
+        #     set_target.target_c = 0.0
+        # if offset >= 2000 and offset < 3500:
+        #     set_target.target_v = -0.015 * ((offset - 1500) // 500)
+        #     set_target.target_h = 0.1
+        #     set_target.target_c = 0.0
+        # if offset >= 3500 and offset < 4000:
+        #     set_target.target_v = 0.0
+        #     set_target.target_h = 0.15
+        #     set_target.target_c = 0.0
+        # if offset >= 4000 and offset < 4500:
+        #     set_target.target_v = 0.0
+        #     set_target.target_h = 0.18
+        #     set_target.target_c = 0.0
+        # if offset >= 4500 and offset < 5000:
+        #     set_target.target_v = 0.01 * ((offset - 4500) // 100)
+        #     set_target.target_h = 0.1
+        #     set_target.target_c = 1.0
+        #
+        # set_target.target_v = min(0.01 * (offset // 400), 0.06)
+        # set_target.target_h = 0.1
+        # set_target.target_c = 0.0
+
+        if offset == 5000:
+            break
+
+
+    # Draw control signal
     signal_num = np.array(all_holder).shape[1]
     for i in range(0, 1):
-        plt.plot([x for x in range(offset)], np.array(all_holder)[:, i], label="control signal "+str(i))
-    plt.legend()
-    plt.show()
+        plt.plot([x for x in range(offset)], np.array(all_holder)[:, i], '-x', label="control signal "+str(i))
+    # plt.legend()
+    # plt.show()
+    # print(velocity_holder)
+    # print(lower_height_holder)
+    # print(upper_height_holder)
+    plt.plot([x for x in range(len(velocity_holder))], velocity_holder, label="v", color='tab:blue')
+    plt.plot([x for x in range(len(velocity_holder))], target_v_holder, label="target v", color='tab:blue', alpha=0.5)
+    # plt.plot([x for x in range(len(velocity_holder))], lower_height_holder, label="h")
+    # plt.plot([x for x in range(len(velocity_holder))], upper_height_holder, label="c")
+    # plt.legend()
+    # plt.show()
+
+    data_dict = {"v": velocity_holder, "target_v": target_v_holder}
+    data_df = pd.DataFrame(data_dict)
+    os.makedirs(f"./stats/robot_{robot_id}/{config_name}", exist_ok=True)
+    task_name = "long"
+    data_df.to_csv(f"./stats/robot_{robot_id}/{config_name}/stats_{task_name}.csv")
+
 
