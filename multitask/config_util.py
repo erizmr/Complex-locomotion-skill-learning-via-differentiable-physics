763b18710965fb8c534e039c28a0961bef3479ec
Mingrui <mingrui.zhang@taichi.graphics>
2021-08-30 13:48:54 +0800

    update robot design

--- multitask/config_util.py
+++ multitask/config_util.py
@@ -19,12 +19,19 @@ def dump_to_json(solver, objects, springs, file = None):
         return str
     json.dump(item, open(file, "w"))
 
+
 if __name__ == "__main__":
     import robot_config
-    robot_builders = robot_config.robots
-    for id, builder in enumerate(robot_builders):
-        obj, spr = builder()
-        dump_to_json("mass_spring", obj, spr, file = os.path.join(current_path(), "robot_configs/{}.json".format(id)))
+    from multitask.robot_design import RobotDesignMassSpring
+    robot_design_file = './robot_design/robot_2.json'
+    robot = RobotDesignMassSpring.from_file(robot_design_file)
+    robot_builders = []
+    robot_builders.append(robot)
+
+    for builder in robot_builders:
+        id = builder.robot_id
+        obj, spr = builder.build()
+        dump_to_json("mass_spring", obj, spr, file=os.path.join(current_path(), "robot_configs/{}.json".format(id)))
         robot_config.clear()
     
     for id in range(5):
