8d9cbde31a28cc034c273cae43df742287574e06
Mingrui <mingrui.zhang@taichi.graphics>
2021-09-30 01:33:06 +0800

    update no target

--- run_scripts_no_target.sh
+++ run_scripts_no_target.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-#for i in 2 3 4 5;
-#do
-#echo $(expr $i);
-#python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot${i}_vhc_5_l01_no_target.json --train
-#done
+for i in 2 3 4 5;
+do
+echo $(expr $i);
+python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot${i}_vhc_5_l01_no_target.json --train
+done
 
 for i in 2 3 4 5;
 do
