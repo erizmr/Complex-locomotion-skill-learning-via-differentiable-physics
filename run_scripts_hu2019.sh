a5182b4dffe24b4a6fb0857b0518b2c98d89606c
Mingrui <mingrui.zhang@taichi.graphics>
2021-10-02 01:16:14 +0800

    add 2019

--- run_scripts_hu2019.sh
+++ run_scripts_hu2019.sh
@@ -1,11 +1,6 @@
-
 #!/bin/bash
-
-python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot2_vhc_5_l01_sgd.json --train
-python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot2_vhc_5_l01_sgd.json --evaluate --no-tensorboard--train --evaluate_path saved_results/sim_config_DiffPhy_robot2_vhc_5_l01_sgd/DiffTaichi_DiffPhy
-
-python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot2_vhc_5_l01_sgd.json --train
-python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot2_vhc_5_l01_sgd.json --evaluate --no-tensorboard--train --evaluate_path saved_results/sim_config_DiffPhy_robot2_vhc_5_l01_sgd/DiffTaichi_DiffPhy
-
-python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot2_vhc_5_l01_sgd.json --train
-python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot2_vhc_5_l01_sgd.json --evaluate --no-tensorboard--train --evaluate_path saved_results/sim_config_DiffPhy_robot2_vhc_5_l01_sgd/DiffTaichi_DiffPhy
+for i in 1 2 3;
+do
+  python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot2_vhc_5_l01_hu2019.json --train
+  python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot2_vhc_5_l01_hu2019.json --evaluate --no-tensorboard--train --evaluate_path saved_results/sim_config_DiffPhy_robot2_vhc_5_l01_hu2019/DiffTaichi_DiffPhy
+done
