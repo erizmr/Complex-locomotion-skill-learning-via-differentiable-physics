46dc7ba4aaeed65402902535da1cdfac7f120ad6
Mingrui <mingrui.zhang@taichi.graphics>
2021-10-02 03:44:50 +0800

    update robot7 scripts

--- scripts/input_analysis.py
+++ scripts/input_analysis.py
@@ -3,7 +3,7 @@ import pickle as pkl
 import matplotlib.pyplot as plt
 import glob
 import numpy as np
-
+robot_names = {2:"Alpaca", 3:"Monster", 4:"HugeStool", 5:"Stool", 6:"Snake"}
 weight_list = []
 def load_weights(name="save.pkl"):
     print(name)
@@ -15,7 +15,8 @@ def load_weights(name="save.pkl"):
 robot_id = 4
 # model_path = glob.glob(f"./saved_results/sim_config_DiffPhy_robot{robot_id}_vhc_5_l05/DiffTaichi_DiffPhy/*/models/weight.pkl")[-1]
 # model_path = "./saved_results/sim_config_DiffPhy_robot4_vhc_5_l05/DiffTaichi_DiffPhy/*/models/weight.pkl"
-model_path = glob.glob(f"./saved_results/sim_config_DiffPhy_robot{robot_id}_vha/DiffTaichi_DiffPhy/*/models/weight.pkl")[-1]
+# model_path = glob.glob(f"./saved_results/sim_config_DiffPhy_robot{robot_id}_vha/DiffTaichi_DiffPhy/*/models/weight.pkl")[-1]
+model_path = glob.glob(f"./saved_results/sim_config_DiffPhy_robot{robot_id}_vh/DiffTaichi_DiffPhy/*/models/weight.pkl")[-1]
 load_weights(model_path)
 weight_1 = weight_list[0]
 weight_2 = weight_list[1]
@@ -41,26 +42,40 @@ import os
 os.makedirs("robot_{}".format(robot_id), exist_ok = True)
 
 def visualize(w, name):
-    sns.heatmap(w, cmap='gray')
-    plt.title(f"robot_{robot_id}_{name}")
-    plt.savefig(f"robot_{robot_id}/{name}.png", dpi = 500)
+    if name == "abs_raw":
+        name = "Weights Absolute Values"
+    if name == "first_k":
+        name = "Top N Values of Rows"
+    ax = sns.heatmap(w, square=True, cbar=True, linewidths=.01, cmap="YlGnBu", cbar_kws={"shrink": .48})
+    # ax.set_title(f"{robot_names[robot_id]} (A) - {name}", fontsize=32)
+    ax.set_title(f"{robot_names[robot_id]} - {name}", fontsize=32)
+    ax.set_xlabel("Input Layer Channel", fontsize=20)
+    ax.set_ylabel("Hidden Layer Channel", fontsize=20)
+    ax.set_yticks(np.array(range(0, 64, 9)) + 0.5)
+    ax.set_yticklabels(range(0, 64, 9), fontsize=18)
+    ax.set_xticklabels(range(0, 202, 8), fontsize=18)
+    cax = plt.gcf().axes[-1]
+    cax.tick_params(labelsize=20)
+    plt.savefig(f"robot_{robot_id}/{name}.png", dpi = 1000)
+    plt.show()
     plt.clf()
+    # exit(0)
 
 visualize(abs_weight_1, "abs_raw")
 
-for i in range(2, 7):
-    visualize(abs_weight_1 > (i / 10.), f"abs_threshold_0.{i}")
-
-visualize(np.floor(abs_weight_1 * 10) / 10 * (abs_weight_1 > 0.2), "abs_floor")
+# for i in range(2, 7):
+#     visualize(abs_weight_1 > (i / 10.), f"abs_threshold_0.{i}")
+#
+# visualize(np.floor(abs_weight_1 * 10) / 10 * (abs_weight_1 > 0.2), "abs_floor")
 
 visualize(vis_first_k, "first_k")
 
-rel_weight_1 = abs_weight_1 / abs_weight_1.max(axis = 1)[:, None]
-abs_weight_1 = None
-visualize(rel_weight_1, "rel_raw")
-assert rel_weight_1.max(axis = 1).mean() > 0.999
-
-for i in range(5, 10):
-    visualize(rel_weight_1 > (i / 10.), f"rel_threshold_0.{i}")
-
-visualize(np.floor(rel_weight_1 * 10) / 10 * (rel_weight_1 > 0.5), "rel_floor")
+# rel_weight_1 = abs_weight_1 / abs_weight_1.max(axis = 1)[:, None]
+# abs_weight_1 = None
+# visualize(rel_weight_1, "rel_raw")
+# assert rel_weight_1.max(axis = 1).mean() > 0.999
+#
+# for i in range(5, 10):
+#     visualize(rel_weight_1 > (i / 10.), f"rel_threshold_0.{i}")
+#
+# visualize(np.floor(rel_weight_1 * 10) / 10 * (rel_weight_1 > 0.5), "rel_floor")
