c5ac7c32f78cd3d3e2044f25e21d4bbca0ff0695
Mingrui <mingrui.zhang@taichi.graphics>
2021-10-13 15:07:55 +0800

    clean scripts

--- run_scripts_validation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-for i in 2 3 4 5;
-do
-echo $(expr $i);
-python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot${i}_vhc.json --evaluate --no-tensorboard --evaluate_path saved_results/sim_config_DiffPhy_robot${i}_vhc/DiffTaichi_DiffPhy
-done
-
