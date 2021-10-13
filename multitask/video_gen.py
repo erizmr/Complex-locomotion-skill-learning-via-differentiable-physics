ffbedb8d033ef09b855e3c03895b11df6ba34279
Mingrui <mingrui.zhang@taichi.graphics>
2021-10-13 14:59:05 +0800

    clean scripts

--- multitask/video_gen.py
+++
@@ -1,5 +0,0 @@
-import os
-os.system("cd video\nrm *.gif")
-dir_list = os.listdir("video/")
-for name in dir_list:
-    os.system("cd video/{}\nti video && ti gif -i video.mp4 -f250 && mv video.gif ../{}.gif".format(name, name))
--- run_scripts_robot7.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Good one
-for i in 1 2 3;
-do
-python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot7_vhc_5_l01.json --train
-python3 main_diff_phy.py --config_file cfg/sim_config_DiffPhy_robot7_vhc_5_l01.json --evaluate --no-tensorboard-train --evaluate_path saved_results/sim_config_DiffPhy_robot7_vhc_5_l01/DiffTaichi_DiffPhy
-done
-
