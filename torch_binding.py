7d293db02d3ca96f2d324260c3bdae2891262efd
Mingrui <mingrui.zhang@taichi.graphics>
2023-04-11 00:32:36 +0800

    update torch binding

--- torch_binding.py
+++ torch_binding.py
@@ -3,7 +3,7 @@ import taichi as ti
 
 from utils import (data_type, ti2torch, ti2torch_grad, ti2torch_grad_vec,
                     ti2torch_vec, torch2ti, torch2ti_grad, torch2ti_grad_vec,
-                    torch2ti_vec, torch2ti_grad_vec3, ti2torch_grad_vec3, torch_type)
+                    torch2ti_vec, torch2ti_grad_vec3, ti2torch_grad_vec3, torch2ti_vec3, ti2torch_vec3, torch_type)
 
 from multitask.robot_design import RobotDesignMassSpring3D
 from grad_implicit_ms_cg import ImplictMassSpringSolver
@@ -15,38 +15,46 @@ class MassSpringSolver(torch.nn.Module):
         self.ms_solver = ImplictMassSpringSolver(robot_builder)
         self.h = 0.01
         self.substeps = 1
+        self.cnt = 0
 
         self.register_buffer(
-            'output_pos',
-            torch.zeros(3, self.ms_solver.NV, dtype=torch_type),
+            'output_dv',
+            torch.zeros(self.ms_solver.NV, 3, dtype=torch_type),
             persistent=False
         )
         self.register_buffer(
-            'action_grad',
-            torch.zeros(self.ms_solver.NE, dtype=torch_type),
+            'grad_rhs',
+            torch.zeros(self.ms_solver.NV, 3, dtype=torch_type),
             persistent=False
         )
         class _module_function(torch.autograd.Function):
 
             @staticmethod
-            def forward(ctx, input_action):
-
-                torch2ti(self.ms_solver.actuation, input_action.contiguous())
-                for i in range(self.substeps):
-                    self.ms_solver.update(self.h)
-                self.output_pos = self.ms_solver.pos.to_torch()
+            def forward(ctx, rhs):
+                self.cnt += 1
+                print("forawrd num ", self.cnt)
+                torch2ti_vec3(self.ms_solver.b, rhs.contiguous())
+                # An initial guess set to zero
+                self.ms_solver.dv.fill(0.0)
+                self.ms_solver.compute_force()
+                self.ms_solver.compute_jacobian()
+                self.ms_solver.cg_solver(self.ms_solver.dv, self.h)
+                ti2torch_vec3(self.ms_solver.dv, self.output_dv.contiguous())
                 
-                return self.output_pos
+                return self.output_dv
 
             @staticmethod
-            def backward(ctx, doutput):                
+            def backward(ctx, grad_output_dv):
+                self.cnt -= 1
+                print("backward num ", self.cnt)           
                 # print(doutput.contiguous().shape)
-                # self.zero_grad()
-                torch2ti_grad_vec3(self.ms_solver.pos, doutput.contiguous())
-                self.ms_solver.update_grad(self.h)
-                ti2torch_grad(self.ms_solver.actuation,
-                                   self.action_grad.contiguous())
-                return self.action_grad
+                self.zero_grad()
+                # print("grad rhs shape ", self.grad_rhs.contiguous().shape)
+                torch2ti_grad_vec3(self.ms_solver.dv, grad_output_dv.contiguous())
+                self.ms_solver.cg_solver_grad(self.ms_solver.dv, self.h)
+                ti2torch_grad_vec3(self.ms_solver.b, self.grad_rhs.contiguous())
+                print("grad_rhs grad ", self.grad_rhs)
+                return self.grad_rhs
 
         self._module_function = _module_function
 
@@ -59,7 +67,7 @@ class MassSpringSolver(torch.nn.Module):
         #     torch.autograd.gradcheck(self._module_function.apply, inputs, eps=1e-2, atol=1e-3, rtol=1.e-3, raise_exception=True)
         # except Exception as e:
         #     print(str(self._module_function.__name__) + " failed: " + str(e))
-        torch.autograd.gradcheck(self._module_function.apply, inputs, eps=1e-2, atol=1e-3, rtol=1.e-3, raise_exception=True)
+        return torch.autograd.gradcheck(self._module_function.apply, inputs, eps=1e-6, atol=1e-3, rtol=1.e-3, raise_exception=True)
 
     def forward(self, input_action):
         return self._module_function.apply(input_action)
